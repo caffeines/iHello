d3b9650aa399620cd67503b08fc887a08e11805c
caffeines <sadat.sayem@tigerit.com>
2021-07-28 00:25:05 +0600

    Add message delivery event

--- server/data/message.py
+++ server/data/message.py
@@ -3,6 +3,7 @@ import sqlalchemy.exc
 from ..database.models import Message
 from ..database.client import db
 from sqlalchemy import or_
+from datetime import datetime
 
 
 def create(sender_id: str, recipient_id: str, message: str):
@@ -30,5 +31,17 @@ def get_by_user_id(user_id: str):
         )
         db.session.commit()
         return messages
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        raise e
+
+
+def set_delivered_at(message_id: str, recipient_id: str):
+    try:
+        now = datetime.utcnow()
+        changed_message = Message.query.filter_by(
+            id=message_id, recipient_id=recipient_id
+        ).update(dict(delivered_at=now))
+        db.session.commit()
+        return changed_message
     except sqlalchemy.exc.SQLAlchemyError as e:
         raise e
--- server/socket_lib/message.py
+++ server/socket_lib/message.py
@@ -23,6 +23,25 @@ def test_disconnect():
     print("Client disconnected")
 
 
+@socketio.on("deliver_message")
+@jwt_required()
+def deliver_message(data):
+    print("message was received: ", data)
+    try:
+        message_id = data["message_id"]
+        recipient_id = get_jwt_identity()
+        delivered = message_dao.set_delivered_at(message_id, recipient_id)
+        return dict(code=200, data=delivered)
+    except TypeError as e:
+        print(e)
+        return dict(code=400, error="Invalid message request data")
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        return dict(code=500, error="Database query failed")
+    except Exception as e:
+        print(e)
+        return dict(code=500, error="Something went wrong")
+
+
 @socketio.on("message")
 @jwt_required()
 def handle_message(msg):
@@ -31,15 +50,18 @@ def handle_message(msg):
         recipient = user.get_by_username(msg["recipient"])
         if recipient is None:
             return dict(code=404, error="User not registered")
+        # TODO: Add contact checker
         new_message = message_dao.create(
             sender_id=user_id, recipient_id=recipient.id, message=msg["message"]
         )
         response = {
+            "id": new_message.id.__str__(),
             "user_id": new_message.sender_id.__str__(),
             "recipient_id": new_message.recipient_id.__str__(),
             "message": new_message.message,
             "created_at": new_message.created_at.__str__(),
         }
+        send(response, to=new_message.recipient_id.__str__())
         return dict(code=200, data=response)
     except TypeError as e:
         print(e)
